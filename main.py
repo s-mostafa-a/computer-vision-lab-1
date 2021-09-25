648577389501ca306373f346df0bbbd17b6b30a4
Mostafa <s.mostafa.a96@gmail.com>
2021-09-25 06:45:23 -0400

    add utils

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 im = np.array(Image.open('./figs/bird.bmp'))
+im = np.array(
+    [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
 print(im.shape)
 plt.axis('off')
 plt.imshow(im)
