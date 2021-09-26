bb30523f6fa06b0eeedf113cba0165cb18302772
Mostafa <s.mostafa.a96@gmail.com>
2021-09-26 16:37:03 -0400

    add the other algorithm

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import get_kernelized_matrix
+from utils import low_pass_filter_using_broadcast, low_pass_filter_without_broadcast
+import time
 
-image = np.array(Image.open('./figs/bird.bmp'))[:, :, 0]
-# image = np.array(
+im = np.array(Image.open('./figs/bird.bmp'))[:, :, 0]
+# im = np.array(
 #     [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
 #      [21, 22, 23, 24, 25]])
 plt.axis('off')
-plt.imshow(image)
+plt.imshow(im)
 plt.show()
 
-kernel = np.ones((3, 3))
-kernel = kernel / np.sum(kernel)
+ker = np.ones((3, 3))
+ker = ker / np.sum(ker)
 
-kernelized_matrix = get_kernelized_matrix(matrix=image, kernel_shape=kernel.shape, mode='same')
-expanded_kernel = np.expand_dims(np.expand_dims(kernel, axis=0), axis=0)
-
-low_path_filtered = np.sum((kernelized_matrix * expanded_kernel), axis=(2, 3))
 plt.axis('off')
-plt.imshow(low_path_filtered)
+start_time = time.time()
+final = low_pass_filter_using_broadcast(im, ker)
+print(f'broadcast {time.time() - start_time} seconds')
+plt.imshow(final)
+plt.show()
+
+plt.axis('off')
+start_time = time.time()
+final = low_pass_filter_without_broadcast(im, ker)
+print(f'without broadcast {time.time() - start_time} seconds')
+plt.imshow(final)
 plt.show()
--- utils.py
+++ utils.py
@@ -1,22 +1,50 @@
 import numpy as np
 
 
-def get_kernelized_matrix(matrix: np.array, kernel_shape: tuple, mode: str):
-    assert len(matrix.shape) == 2, "The image must be a 2d image!"
-    assert len(kernel_shape) == 2, "The kernel must be 2d!"
+def _assertion_checks(image, kernel, mode):
+    assert len(image.shape) == 2, "The image must be a 2d image!"
+    assert len(kernel.shape) == 2, "The kernel must be 2d!"
     assert mode in ("valid", "same"), "The mode parameter can only be `valid` or `same`"
-    assert kernel_shape[0] % 2 == 1 and kernel_shape[
+    assert kernel.shape[0] % 2 == 1 and kernel.shape[
         1] % 2 == 1, "kernel shape must be a tuple of odd numbers!"
+
+
+def _get_paddings_and_new_image(image, kernel_shape, mode):
     padding_i = kernel_shape[0] // 2
     padding_j = kernel_shape[1] // 2
     if mode == "same":
-        matrix = np.pad(matrix, [(padding_i,), (padding_j,)])
-    shape = tuple([matrix.shape[0] - 2 * padding_i, matrix.shape[1] - 2 * padding_j] +
-                  list(kernel_shape))
-    kernelized_matrix = np.empty(shape=shape, dtype=matrix.dtype)
-    for i in range(padding_i, matrix.shape[0] - padding_i):
-        for j in range(padding_j, matrix.shape[1] - padding_j):
-            kernelized_matrix[i - padding_i, j - padding_j] = matrix[
-                                                              i - padding_i:i + padding_i + 1,
-                                                              j - padding_j:j + padding_j + 1]
-    return kernelized_matrix
+        image = np.pad(image, [(padding_i,), (padding_j,)])
+    return padding_i, padding_j, image
+
+
+def low_pass_filter_using_broadcast(image: np.array, kernel: np.array, mode: str = 'same'):
+    _assertion_checks(image=image, kernel=kernel, mode=mode)
+    padding_i, padding_j, image = _get_paddings_and_new_image(image=image,
+                                                              kernel_shape=kernel.shape, mode=mode)
+    shape = tuple([image.shape[0] - 2 * padding_i, image.shape[1] - 2 * padding_j] +
+                  list(kernel.shape))
+    multi_dim_image = np.empty(shape=shape, dtype=image.dtype)
+    for i in range(padding_i, image.shape[0] - padding_i):
+        for j in range(padding_j, image.shape[1] - padding_j):
+            multi_dim_image[i - padding_i, j - padding_j] = image[
+                                                            i - padding_i:i + padding_i + 1,
+                                                            j - padding_j:j + padding_j + 1]
+
+    expanded_kernel = np.expand_dims(np.expand_dims(kernel, axis=0), axis=0)
+
+    final_image = np.sum((multi_dim_image * expanded_kernel), axis=(2, 3))
+    return final_image
+
+
+def low_pass_filter_without_broadcast(image: np.array, kernel: np.array, mode: str = 'same'):
+    _assertion_checks(image=image, kernel=kernel, mode=mode)
+    padding_i, padding_j, image = _get_paddings_and_new_image(image=image,
+                                                              kernel_shape=kernel.shape, mode=mode)
+    shape = (image.shape[0] - 2 * padding_i, image.shape[1] - 2 * padding_j)
+    final_image = np.empty(shape=shape, dtype=image.dtype)
+    for i in range(padding_i, image.shape[0] - padding_i):
+        for j in range(padding_j, image.shape[1] - padding_j):
+            summation = np.sum(
+                image[i - padding_i:i + padding_i + 1, j - padding_j:j + padding_j + 1] * kernel)
+            final_image[i - padding_i, j - padding_j] = summation
+    return final_image
